d953c1a7764eb69168475afdd1a2e92a962451d1
Eduardo <dudarenz@gmail.com>
2023-03-24 00:14:10 -0300

    update check db host

--- database/db.go
+++ database/db.go
@@ -17,7 +17,11 @@ var (
 
 func ConectaComBancoDeDados() {
 	envFile, _ := godotenv.Read(".env")
-	stringDeConexao := "host="+envFile["DATABASE_HOST"]+" user=root password=root dbname=root port=5432 sslmode=disable"
+	host := envFile["DATABASE_HOST"]
+	if(host == ""){
+		host = "localhost"
+	}
+	stringDeConexao := "host="+host+" user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
